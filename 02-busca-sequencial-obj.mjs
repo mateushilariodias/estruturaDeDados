fe6e387114a5918fb546bc33cc6a1346ae977031
mateushilariodias <mateusilariodias@gmail.com>
2023-03-01 08:17:04 -0300

    Fixed the object sequential search code

--- 02-busca-sequencial-obj.mjs
+++ 02-busca-sequencial-obj.mjs
@@ -5,10 +5,14 @@ function buscaSequencial(array, funcaoDeComparacao) {
     return -1
 }
 
-function comparaNome(obj) {
-    return obj.first_name === "LUCAS"
-}
+// function comparaNome(obj) {
+//     return obj.first_name === "LUCAS"
+// }
 
 import { objNomes } from './data/vetor-obj-nomes.mjs'
 
-console.log("Posição de LUCAS:", buscaSequencial(objNomes, comparaNome))
+//console.log("Posição de LUCAS:", buscaSequencial(objNomes, comparaNome))
+
+console.log("Posição de frequency_total 14909:", buscaSequencial(objNomes, function(obj) {
+    return obj.frequency_total === 14909
+}))
