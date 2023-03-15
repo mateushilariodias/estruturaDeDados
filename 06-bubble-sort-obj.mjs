08045e638c812e856f3224ce1997ba864c52e4b3
mateushilariodias <mateusilariodias@gmail.com>
2023-03-15 09:19:44 -0300

    added 06-bublle-sort-obj.mjs

---
+++ 06-bubble-sort-obj.mjs
@@ -0,0 +1,33 @@
+function bubbleSort(array, funcaoDeComparacao) {
+    let swap
+
+    do {
+        swap = false
+        for (let i = 0; i < array.length - 1; i++) {
+            if(funcaoDeComparacao(array[i], array[i+1])) {
+                [ array[i], array[i+1] ] = [ array[i+1], array[i] ]
+                swap = true
+            }
+        }
+    } while(swap)
+}
+
+import { objMotoristas } from './data/motoristas-obj-desord.mjs'
+
+//Ordenação crescente
+console.time('Tempo de ordenação')
+bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista > elementTwo.nome_motorista)
+console.timeEnd('Tempo de ordenação')
+console.log(objMotoristas)
+
+//Ordenação decrescente
+console.time('Tempo de ordenação')
+bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista < elementTwo.nome_motorista)
+console.timeEnd('Tempo de ordenação')
+console.log(objMotoristas)
+
+//Ordenação decrescente ignorando acentos
+console.time('Tempo de ordenação')
+bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista.localeCompare( elementTwo.nome_motorista, 'pt-br' ) <= 0)
+console.timeEnd('Tempo de ordenação')
+console.log(objMotoristas)
