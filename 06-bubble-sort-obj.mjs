2554c234fe270fc65aa5a99834cce4ca17875994
mateushilariodias <mateusilariodias@gmail.com>
2023-03-18 17:45:45 -0300

    Added some stuff in the bublle-sort-obj.mjs file

--- 06-bubble-sort-obj.mjs
+++ 06-bubble-sort-obj.mjs
@@ -15,19 +15,30 @@ function bubbleSort(array, funcaoDeComparacao) {
 import { objMotoristas } from './data/motoristas-obj-desord.mjs'
 
 //Ordenação crescente
-console.time('Tempo de ordenação')
-bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista > elementTwo.nome_motorista)
-console.timeEnd('Tempo de ordenação')
-console.log(objMotoristas)
+// console.time('Tempo de ordenação')
+// bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista > elementTwo.nome_motorista)
+// console.timeEnd('Tempo de ordenação')
+// console.log(objMotoristas)
 
 //Ordenação decrescente
-console.time('Tempo de ordenação')
-bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista < elementTwo.nome_motorista)
-console.timeEnd('Tempo de ordenação')
-console.log(objMotoristas)
+// console.time('Tempo de ordenação')
+// bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista < elementTwo.nome_motorista)
+// console.timeEnd('Tempo de ordenação')
+// console.log(objMotoristas)
 
 //Ordenação decrescente ignorando acentos
-console.time('Tempo de ordenação')
-bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista.localeCompare( elementTwo.nome_motorista, 'pt-br' ) <= 0)
-console.timeEnd('Tempo de ordenação')
+// console.time('Tempo de ordenação')
+// bubbleSort(objMotoristas, (elementOne, elementTwo) => elementOne.nome_motorista.localeCompare( elementTwo.nome_motorista, 'pt-br' ) <= 0)
+// console.timeEnd('Tempo de ordenação')
+// console.log(objMotoristas)
+
+
+//Ordenação Dois Níveis: primeiro por razão_social, depois por nome_motorista
+bubbleSort(objMotoristas, (elementOne, elementTwo) => {
+    if(elementOne.razao_social === elementTwo.razao_social){
+        return elementOne.nome_motorista > elementTwo.nome_motorista
+        }
+        return elementOne.razao_social > elementTwo.razao_social
+})
+
 console.log(objMotoristas)
